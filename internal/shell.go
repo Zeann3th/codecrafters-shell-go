948e4e9ae841d6a63ca77ddaf2c31e69a72257da
zeann3th <longhienly112@gmail.com>
2025-02-02 19:43:08 +0700

    feat: redirection

--- internal/shell.go
+++ internal/shell.go
@@ -11,21 +11,26 @@ import (
 	"strings"
 )
 
+type CommandFunc func(args []string, next CommandFunc) error
+
 type Shell struct {
-	queue    []Command
-	commands map[string]func(args []string)
+	stack    []Command
+	commands map[string]CommandFunc
 	aliases  map[string]string
 }
 
 type Command struct {
-	op   string
-	args []string
+	op          string
+	args        []string
+	stdout      string
+	stderr      string
+	nextCommand *Command
 }
 
 func NewShell() *Shell {
 	s := &Shell{
-		queue:    []Command{},
-		commands: make(map[string]func(args []string)),
+		stack:    []Command{},
+		commands: make(map[string]CommandFunc),
 		aliases:  map[string]string{"~": os.Getenv("HOME")},
 	}
 	s.initCommands()
@@ -57,126 +62,193 @@ func (s *Shell) Run() {
 			continue
 		}
 
-		op, args := s.parse(command)
-
-		if cmd, exists := s.commands[op]; exists {
-			cmd(args)
-		} else {
-			ext := exec.Command(op, args...)
-			ext.Stderr = os.Stderr
-			ext.Stdout = os.Stdout
-
-			err := ext.Run()
-			if err != nil {
-				fmt.Println(command + ": command not found")
-			}
+		s.parseCommand(command)
+		if len(s.stack) > 0 {
+			s.executeCommand(s.stack[0])
+			// Clear stack after execution
+			s.stack = []Command{}
 		}
 	}
 }
 
-func (s *Shell) parse(command string) (string, []string) {
-	var args []string
-	var current strings.Builder
+func (s *Shell) parseCommand(input string) {
+	var current Command
+	var current_token strings.Builder
 	var singleQuote, doubleQuote, backslash bool
 	isFirst := true
-	var op string
 
-	for _, c := range command {
-		switch c {
-		case '\'':
-			if backslash && doubleQuote {
-				current.WriteRune('\\')
-			}
-			if backslash || doubleQuote {
-				current.WriteRune(c)
+	s.stack = []Command{}
+
+	flushToken := func() {
+		if current_token.Len() > 0 {
+			if isFirst {
+				current.op = current_token.String()
+				isFirst = false
 			} else {
+				current.args = append(current.args, current_token.String())
+			}
+			current_token.Reset()
+		}
+	}
+
+	pushCommand := func() {
+		flushToken()
+		if current.op != "" {
+			s.stack = append(s.stack, current)
+			current = Command{}
+			isFirst = true
+		}
+	}
+
+	for i := 0; i < len(input); i++ {
+		c := rune(input[i])
+
+		switch {
+		case backslash:
+			current_token.WriteRune(c)
+			backslash = false
+			continue
+		case c == '\\':
+			backslash = true
+			continue
+		case c == '\'':
+			if !doubleQuote {
 				singleQuote = !singleQuote
+				continue
 			}
-			backslash = false
-		case '"':
-			if backslash || singleQuote {
-				current.WriteRune(c)
-			} else {
+		case c == '"':
+			if !singleQuote {
 				doubleQuote = !doubleQuote
+				continue
 			}
-			backslash = false
-		case '\\':
-			if backslash || singleQuote {
-				current.WriteRune(c)
-				backslash = false
-			} else {
-				backslash = true
-			}
-		case ' ':
-			if backslash && doubleQuote {
-				current.WriteRune('\\')
-			}
-			if backslash || singleQuote || doubleQuote {
-				current.WriteRune(c)
-			} else if current.Len() > 0 {
-				if isFirst {
-					op = current.String()
-					isFirst = false
+		}
+
+		if !singleQuote && !doubleQuote {
+			// Handle special operators
+			if c == '>' {
+				flushToken()
+				// Check for 2> or 1>
+				if i > 0 && (input[i-1] == '2' || input[i-1] == '1') {
+					if input[i-1] == '2' {
+						current.stderr = strings.TrimSpace(input[i+1:])
+					} else {
+						current.stdout = strings.TrimSpace(input[i+1:])
+					}
 				} else {
-					args = append(args, current.String())
+					current.stdout = strings.TrimSpace(input[i+1:])
 				}
-				current.Reset()
+				pushCommand()
+				break
+			} else if i < len(input)-1 && c == '&' && input[i+1] == '&' {
+				pushCommand()
+				i++
+				continue
 			}
-			backslash = false
-		default:
-			if doubleQuote && backslash {
-				current.WriteRune('\\')
-			}
-			current.WriteRune(c)
-			backslash = false
 		}
-	}
 
-	if current.Len() > 0 {
-		if isFirst {
-			op = current.String()
+		if c == ' ' && !singleQuote && !doubleQuote {
+			flushToken()
 		} else {
-			args = append(args, current.String())
+			current_token.WriteRune(c)
 		}
 	}
 
-	return op, args
+	pushCommand()
+
+	for i := 0; i < len(s.stack)-1; i++ {
+		s.stack[i].nextCommand = &s.stack[i+1]
+	}
 }
 
-func (s *Shell) exit(args []string) {
+func (s *Shell) executeCommand(cmd Command) error {
+	var nextFunc CommandFunc
+	if cmd.nextCommand != nil {
+		nextFunc = func(args []string, _ CommandFunc) error {
+			return s.executeCommand(*cmd.nextCommand)
+		}
+	}
+
+	if shellCmd, exists := s.commands[cmd.op]; exists {
+		return shellCmd(cmd.args, nextFunc)
+	} else {
+		return s.executeExternal(cmd, nextFunc)
+	}
+}
+
+func (s *Shell) executeExternal(cmd Command, next CommandFunc) error {
+	ext := exec.Command(cmd.op, cmd.args...)
+
+	if cmd.stdout != "" {
+		file, err := os.Create(cmd.stdout)
+		if err != nil {
+			return fmt.Errorf("Error creating output file: %v", err)
+		}
+		defer file.Close()
+		ext.Stdout = file
+	} else {
+		ext.Stdout = os.Stdout
+	}
+
+	if cmd.stderr != "" {
+		file, err := os.Create(cmd.stderr)
+		if err != nil {
+			return fmt.Errorf("Error creating error file: %v", err)
+		}
+		defer file.Close()
+		ext.Stderr = file
+	} else {
+		ext.Stderr = os.Stderr
+	}
+
+	err := ext.Run()
+	if err != nil {
+		return fmt.Errorf("%s: command not found", cmd.op)
+	}
+
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
+}
+
+func (s *Shell) exit(args []string, next CommandFunc) error {
 	if len(args) > 1 {
-		fmt.Println("Error: Expected [0:1] argument, received " + strconv.Itoa(len(args)))
-		return
+		return fmt.Errorf("Error: Expected [0:1] argument, received %d", len(args))
 	} else if len(args) == 0 {
 		os.Exit(0)
 	} else {
 		code, err := strconv.Atoi(args[0])
 		if err != nil {
-			fmt.Println(fmt.Errorf("Error: %v", err))
-			return
+			return err
 		}
 		os.Exit(code)
 	}
+	return nil
 }
 
-func (s *Shell) echo(args []string) {
+func (s *Shell) echo(args []string, next CommandFunc) error {
 	fmt.Println(strings.Join(args, " "))
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
 }
 
-func (s *Shell) _type(args []string) {
+func (s *Shell) _type(args []string, next CommandFunc) error {
 	if len(args) != 1 {
-		fmt.Println("Error: Expected 1 argument, received " + strconv.Itoa(len(args)))
-		return
+		return fmt.Errorf("Error: Expected 1 argument, received %d", len(args))
 	}
 	if _, exists := s.commands[args[0]]; exists {
 		fmt.Println(args[0] + " is a shell builtin")
-		return
-	}
-	if fp, exists := find(args[0]); exists {
+	} else if fp, exists := find(args[0]); exists {
 		fmt.Println(args[0] + " is " + fp)
-		return
+	} else {
+		fmt.Println(args[0] + ": not found")
 	}
-	fmt.Println(args[0] + ": not found")
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
 }
 
 func find(exe string) (string, bool) {
@@ -190,17 +262,19 @@ func find(exe string) (string, bool) {
 	return "NOENT", false
 }
 
-func (s *Shell) pwd(args []string) {
+func (s *Shell) pwd(args []string, next CommandFunc) error {
 	path, err := os.Getwd()
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		return err
 	}
 	fmt.Println(path)
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
 }
 
-func (s *Shell) clear(args []string) {
-	_ = args
+func (s *Shell) clear(args []string, next CommandFunc) error {
 	switch runtime.GOOS {
 	case "linux":
 		cmd := exec.Command("clear")
@@ -211,8 +285,26 @@ func (s *Shell) clear(args []string) {
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	default:
-		fmt.Println("Error: Unsupported OS")
+		return fmt.Errorf("Error: Unsupported OS")
 	}
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
+}
+
+func (s *Shell) cd(args []string, next CommandFunc) error {
+	if len(args) == 0 {
+		return fmt.Errorf("Error: No directory specified")
+	}
+	err := os.Chdir(s.replacePath(args[0]))
+	if err != nil {
+		return fmt.Errorf("cd: %v: No such file or directory", args[0])
+	}
+	if next != nil {
+		return next(nil, nil)
+	}
+	return nil
 }
 
 func (s *Shell) replacePath(path string) string {
@@ -220,15 +312,4 @@ func (s *Shell) replacePath(path string) string {
 		path = strings.Replace(path, alias, origin, 1)
 	}
 	return path
-}
-
-func (s *Shell) cd(args []string) {
-	if len(args) == 0 {
-		fmt.Println("Error: No directory specified")
-		return
-	}
-	err := os.Chdir(s.replacePath(args[0]))
-	if err != nil {
-		fmt.Printf("cd: %v: No such file or directory\n", args[0])
-	}
 }
